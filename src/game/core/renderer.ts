74463e3b1d97c67985073bf6556497df7bde9eb6
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-24 13:18:40 +0100

    feat(entity): add `drawRect` to renderer

--- src/game/core/renderer.ts
+++ src/game/core/renderer.ts
@@ -9,6 +9,11 @@ export class Renderer {
         this.ctx.clearRect(0, 0, this.width, this.height);
     }
 
+    drawRect(x: number, y: number, w: number, h: number, c: string) {
+        this.ctx.fillStyle = c;
+        this.ctx.fillRect(x, y, w, h);
+    }
+
     get width() {
         return this.canvas.width;
     }
