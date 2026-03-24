6349e8011ceb74340110150fa7564a5fefa0993f
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-24 18:26:57 +0100

    chore(games): add code to example-game2

--- src/games/example-game2/main.ts
+++ src/games/example-game2/main.ts
@@ -1,1 +1,19 @@
-1
+import {createGamePage} from "../../engine/bootstrap.ts";
+import {Game} from "../../engine/core/game.ts";
+import type {Renderer} from "../../engine/core/renderer.ts";
+import {Screen} from "../../engine/screens/screen.ts";
+
+class ExampleGame1 extends Game {
+    constructor() {
+        super();
+
+        this.screen = new class extends Screen {
+            update() {}
+            render(renderer: Renderer) {
+                renderer.drawRect(0, 0, renderer.width, renderer.height, "#00ff00")
+            }
+        }
+    }
+}
+
+createGamePage(ExampleGame1)
