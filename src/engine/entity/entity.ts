d82a12dab5aa47ab31c36053e730d9bef92421ec
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-24 18:49:05 +0100

    feat(entity): add collision

--- src/engine/entity/entity.ts
+++ src/engine/entity/entity.ts
@@ -5,13 +5,22 @@ export abstract class Entity implements Vector2Like {
     x: number;
     y: number;
     w: number;
-    h: number;  
+    h: number;
 
     protected constructor(x: number, y: number, w: number, h: number) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+    }
+
+    collidesWith(other: Entity) {
+        return (
+            this.x < other.x + other.w &&
+            this.x + this.w > other.x &&
+            this.y < other.y + other.h &&
+            this.y + this.h > other.y
+        );
     }
 
     update(delta: number) {}
