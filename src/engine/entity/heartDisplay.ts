4a75e8fd6dc77293f77c5ff8725d8a114fa301d8
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-26 09:29:38 +0100

    fix(ui): fix in heartDisplay

--- src/engine/entity/heartDisplay.ts
+++ src/engine/entity/heartDisplay.ts
@@ -8,7 +8,7 @@ export class HeartDisplay extends Entity {
     static readonly gap: number = 5
     static readonly heartSize: number = 15
 
-    constructor(public lifes: number = 2) {
+    constructor(public lifes: number = 3) {
         super(
             config.canvas_width - HeartDisplay.padding - HeartDisplay.maxLifes * HeartDisplay.heartSize - (HeartDisplay.maxLifes - 1) * HeartDisplay.gap,
             HeartDisplay.padding,
@@ -19,7 +19,7 @@ export class HeartDisplay extends Entity {
 
     render(r: Renderer) {
         for (let i = 0; i < HeartDisplay.maxLifes; i++) {
-            if (this.lifes >= i) {
+            if (this.lifes >= i + 1) {
                 r.drawRect(this.x + (HeartDisplay.heartSize + HeartDisplay.gap) * i, this.y, HeartDisplay.heartSize, HeartDisplay.heartSize, "#f00")
             } else {
                 r.drawRect(this.x + (HeartDisplay.heartSize + HeartDisplay.gap) * i, this.y, HeartDisplay.heartSize, HeartDisplay.heartSize, "#444")
